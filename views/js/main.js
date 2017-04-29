2d191d75e1fd0df7e9bea868bfc7cd05db4e4b2a
felicityky <fingers160308@gmail.com>
2017-04-29 15:13:00 +0800

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -564,7 +564,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  var num = Window.innerHeight/100 * 6;
+  var num = Window.innerHeight/100;
   for (var i = 0; i < num; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
